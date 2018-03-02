14382ec7bac49c90a73269f3a2d7bff9c17accc7
Soby Chacko <schacko@pivotal.io>
2018-03-02 11:09:04 -0500

    samples cleanup

--- source/src/test/java/demo/ModuleApplicationTests.java
+++ source/src/test/java/demo/ModuleApplicationTests.java
@@ -22,7 +22,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class ModuleApplicationTests {
 
 	@Test
