a2f642ebe40c7937b8065e0a86e72712444afce8
Gary Russell <grussell@pivotal.io>
2018-04-05 15:51:52 -0400

    GH-65: Add polled-consumer Sample
    Resolves https://github.com/spring-cloud/spring-cloud-stream-samples/issues/65
    
    Polishing - PR Comments
    
    - add docker-compose
    - change runner to process multiple messages

---
+++ processor-samples/polled-consumer/README.md
@@ -0,0 +1,57 @@
+Spring Cloud Stream Polled Consumer with Embedded Kafka Broker Sample
+=====================================================================
+
+In this *Spring Cloud Stream* sample, an embedded kafka broker is used for testing a polled consumer.
+
+## Requirements
+
+To run this sample, you will need to have installed:
+
+* Java 8 or Above
+
+## Code Tour
+
+This sample is a Spring Boot application that uses Spring Cloud Stream to receive a `String` message and forward an upper case version of that String; it uses the Kafka binder.
+It uses a Polled Consumer (rather than a message-driven `@StreamListener`).
+
+* PolledConsumerApplication - the Spring Boot Main Application
+* PolledConsumerApplicationTests - the test case
+
+The `spring-kafka-test` dependency added to the `pom.xml` puts the `KafkaEmbedded` JUnit `@Rule` on the class path.
+Refer to the [Spring for Apache Kafka Reference Manual](http://docs.spring.io/spring-kafka/reference/htmlsingle/#testing) for more information about this.
+Notice how the `@BeforeClass` method sets up the Boot and binder properties to locate the servers.
+
+See the test method for the details on interacting with Embedded Kafka.
+The test method starts by sending a message to the input destination.
+It then creates a consumer to consume from the output destination; gets the output message and asserts that it's an upper case version of the sent message.
+
+## Building with Maven
+
+Build the sample and run the test by executing:
+
+`./mvnw clean package`
+
+or run the test in your favorite IDE.
+
+=== Running the app:
+
+Go to the root of the repository and do:
+
+`docker-compose up -d`
+
+`./mvnw clean package`
+
+`java -jar target/polled-consumer-0.0.1-SNAPSHOT.jar`
+
+Then send/receive messages to/from the topics.
+
+`$ docker exec -it kafka-polledconsumer /opt/kafka/bin/kafka-console-producer.sh --broker-list 127.0.0.1:9092 --topic polledConsumerIn`
+
+On another terminal:
+
+and
+
+`$ docker exec -it kafka-polledconsumer /opt/kafka/bin/kafka-console-consumer.sh --bootstrap-server 127.0.0.1:9092 --topic polledConsumerOut --from-beginning`
+
+Once you are done testing, stop the Kafka cluster: `docker-compose down`
+
---
+++ processor-samples/polled-consumer/src/main/java/demo/PolledConsumerApplication.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package demo;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.cloud.stream.binder.PollableMessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
+
+/**
+ * Sample app demonstrating a polled consumer where the application can
+ * control the rate at which messages are retrieved.
+ *
+ * @author Gary Russell
+ *
+ */
+@SpringBootApplication
+@EnableBinding(PolledConsumerApplication.PolledProcessor.class)
+public class PolledConsumerApplication {
+
+	public static final ExecutorService exec = Executors.newSingleThreadExecutor();
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolledConsumerApplication.class, args);
+	}
+
+	@Bean
+	public ApplicationRunner runner(PollableMessageSource input, MessageChannel output) {
+		return args -> {
+			System.out.println("Send some messages to topic polledConsumerIn and receive from polledConsumerOut");
+			System.out.println("Messages will be processed one per second");
+			exec.execute(() -> {
+				boolean result = false;
+				while (true) {
+					// this is where we poll for a message, process it, and send a new one
+					result = input.poll(m -> {
+						String payload = (String) m.getPayload();
+						System.out.println("Received: " + payload);
+						output.send(MessageBuilder.withPayload(payload.toUpperCase())
+							.copyHeaders(m.getHeaders())
+							.build());
+					}, new ParameterizedTypeReference<String>() { });
+
+					try {
+						Thread.sleep(1_000);
+					}
+					catch (InterruptedException e) {
+						Thread.currentThread().interrupt();
+						break;
+					}
+					if (result) {
+						System.out.println("Success");
+					}
+				}
+			});
+		};
+	}
+
+	public interface PolledProcessor {
+
+		@Input
+		PollableMessageSource input();
+
+		@Output
+		MessageChannel output();
+
+	}
+
+}
