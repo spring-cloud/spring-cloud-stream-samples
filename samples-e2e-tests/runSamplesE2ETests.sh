e2111652fd09f5a2c674b899bfef10537dae4090
Soby Chacko <schacko@pivotal.io>
2018-08-17 21:02:30 -0400

    More structural changes to samples repo
    Remove CF Acceptance tests
    Convert local sample tests as e2e tests for the various sample apps
    Polishing

--- samples-e2e-tests/runSamplesE2ETests.sh
+++ samples-e2e-tests/runSamplesE2ETests.sh
@@ -13,11 +13,11 @@ function prepare_jdbc_source_with_kafka_and_rabbit_binders() {
  pushd ../source-samples/jdbc-source
 ./mvnw clean package -U -DskipTests
 
-cp target/jdbc-source-*-SNAPSHOT.jar /tmp/jdbc-source-kafka-sample.jar
+cp target/sample-jdbc-source-*-SNAPSHOT-kafka.jar /tmp/jdbc-source-kafka-sample.jar
 
 ./mvnw clean package -U -P rabbit-binder -DskipTests
 
-cp target/jdbc-source-*-SNAPSHOT.jar /tmp/jdbc-source-rabbit-sample.jar
+cp target/sample-jdbc-source-*-SNAPSHOT-rabbit.jar /tmp/jdbc-source-rabbit-sample.jar
 
 popd
 
@@ -27,11 +27,11 @@ function prepare_jdbc_sink_with_kafka_and_rabbit_binders() {
  pushd ../sink-samples/jdbc-sink
 ./mvnw clean package -U -DskipTests
 
-cp target/jdbc-sink-*-SNAPSHOT.jar /tmp/jdbc-sink-kafka-sample.jar
+cp target/sample-jdbc-sink-*-SNAPSHOT-kafka.jar /tmp/jdbc-sink-kafka-sample.jar
 
 ./mvnw clean package -U -P rabbit-binder -DskipTests
 
-cp target/jdbc-sink-*-SNAPSHOT.jar /tmp/jdbc-sink-rabbit-sample.jar
+cp target/sample-jdbc-sink-*-SNAPSHOT-rabbit.jar /tmp/jdbc-sink-rabbit-sample.jar
 
 popd
 
@@ -41,11 +41,11 @@ function prepare_dynamic_source_with_kafka_and_rabbit_binders() {
  pushd ../source-samples/dynamic-destination-source
 ./mvnw clean package -U -DskipTests
 
-cp target/dynamic-destination-source-*-SNAPSHOT.jar /tmp/dynamic-destination-source-kafka-sample.jar
+cp target/dynamic-destination-source-*-SNAPSHOT-kafka.jar /tmp/dynamic-destination-source-kafka-sample.jar
 
 ./mvnw clean package -U -P rabbit-binder -DskipTests
 
-cp target/dynamic-destination-source-*-SNAPSHOT.jar /tmp/dynamic-destination-source-rabbit-sample.jar
+cp target/dynamic-destination-source-*-SNAPSHOT-rabbit.jar /tmp/dynamic-destination-source-rabbit-sample.jar
 
 popd
 
@@ -85,11 +85,11 @@ function prepare_streamlistener_basic_with_kafka_rabbit_binders() {
 pushd ../processor-samples/streamlistener-basic
 ./mvnw clean package -U -DskipTests
 
-cp target/streamlistener-basic-*-SNAPSHOT.jar /tmp/streamlistener-basic-kafka-sample.jar
+cp target/streamlistener-basic-*-SNAPSHOT-kafka.jar /tmp/streamlistener-basic-kafka-sample.jar
 
 ./mvnw clean package -U -P rabbit-binder -DskipTests
 
-cp target/streamlistener-basic-*-SNAPSHOT.jar /tmp/streamlistener-basic-rabbit-sample.jar
+cp target/streamlistener-basic-*-SNAPSHOT-rabbit.jar /tmp/streamlistener-basic-rabbit-sample.jar
 
 popd
 
@@ -99,11 +99,11 @@ function prepare_reactive_processor_with_kafka_rabbit_binders() {
 pushd ../processor-samples/reactive-processor
 ./mvnw clean package -U -DskipTests
 
-cp target/reactive-processor-*-SNAPSHOT.jar /tmp/reactive-processor-kafka-sample.jar
+cp target/reactive-processor-*-SNAPSHOT-kafka.jar /tmp/reactive-processor-kafka-sample.jar
 
 ./mvnw clean package -U -P rabbit-binder -DskipTests
 
-cp target/reactive-processor-*-SNAPSHOT.jar /tmp/reactive-processor-rabbit-sample.jar
+cp target/reactive-processor-*-SNAPSHOT-rabbit.jar /tmp/reactive-processor-rabbit-sample.jar
 
 popd
 
@@ -113,11 +113,11 @@ function prepare_sensor_average_reactive_with_kafka_rabbit_binders() {
 pushd ../processor-samples/sensor-average-reactive
 ./mvnw clean package -U -DskipTests
 
-cp target/sensor-average-reactive-*-SNAPSHOT.jar /tmp/sensor-average-reactive-kafka-sample.jar
+cp target/sensor-average-reactive-*-SNAPSHOT-kafka.jar /tmp/sensor-average-reactive-kafka-sample.jar
 
 ./mvnw clean package -U -P rabbit-binder -DskipTests
 
-cp target/sensor-average-reactive-*-SNAPSHOT.jar /tmp/sensor-average-reactive-rabbit-sample.jar
+cp target/sensor-average-reactive-*-SNAPSHOT-rabbit.jar /tmp/sensor-average-reactive-rabbit-sample.jar
 
 popd
 
@@ -127,17 +127,17 @@ function prepare_schema_registry_vanilla_with_kafka_rabbit_binders() {
 pushd ../schema-registry-samples/schema-registry-vanilla
 ./mvnw clean package -U -DskipTests
 
-cp registry/target/registry-*-SNAPSHOT.jar /tmp/schema-registry-vanilla-registry-kafka.jar
-cp consumer/target/consumer-*-SNAPSHOT.jar /tmp/schema-registry-vanilla-consumer-kafka.jar
-cp producer1/target/producer1-*-SNAPSHOT.jar /tmp/schema-registry-vanilla-producer1-kafka.jar
-cp producer2/target/producer2-*-SNAPSHOT.jar /tmp/schema-registry-vanilla-producer2-kafka.jar
+cp schema-registry-vanilla-server/target/schema-registry-vanilla-server-*-SNAPSHOT.jar /tmp/schema-registry-vanilla-registry-kafka.jar
+cp schema-registry-vanilla-consumer/target/schema-registry-vanilla-consumer-*-kafka.jar /tmp/schema-registry-vanilla-consumer-kafka.jar
+cp schema-registry-vanilla-producer1/target/schema-registry-vanilla-producer1-*-kafka.jar /tmp/schema-registry-vanilla-producer1-kafka.jar
+cp schema-registry-vanilla-producer2/target/schema-registry-vanilla-producer2-*-kafka.jar /tmp/schema-registry-vanilla-producer2-kafka.jar
 
 ./mvnw clean package -U -P rabbit-binder -DskipTests
 
-cp registry/target/registry-*-SNAPSHOT.jar /tmp/schema-registry-vanilla-registry-rabbit.jar
-cp consumer/target/consumer-*-SNAPSHOT.jar /tmp/schema-registry-vanilla-consumer-rabbit.jar
-cp producer1/target/producer1-*-SNAPSHOT.jar /tmp/schema-registry-vanilla-producer1-rabbit.jar
-cp producer2/target/producer2-*-SNAPSHOT.jar /tmp/schema-registry-vanilla-producer2-rabbit.jar
+cp schema-registry-vanilla-server/target/schema-registry-vanilla-server-*-SNAPSHOT.jar /tmp/schema-registry-vanilla-registry-rabbit.jar
+cp schema-registry-vanilla-consumer/target/schema-registry-vanilla-consumer-*-rabbit.jar /tmp/schema-registry-vanilla-consumer-rabbit.jar
+cp schema-registry-vanilla-producer1/target/schema-registry-vanilla-producer1-*-rabbit.jar /tmp/schema-registry-vanilla-producer1-rabbit.jar
+cp schema-registry-vanilla-producer2/target/schema-registry-vanilla-producer2-*-rabbit.jar /tmp/schema-registry-vanilla-producer2-rabbit.jar
 
 popd
 
@@ -147,13 +147,13 @@ function prepare_partitioning_with_kafka_rabbit_binders() {
 pushd ../partitioning-samples
 ./mvnw clean package -U -DskipTests
 
-cp partitioning-producer/target/partitioning-producer-*-SNAPSHOT.jar /tmp/partitioning-producer-kafka.jar
-cp partitioning-consumer-kafka/target/partitioning-consumer-kafka-*-SNAPSHOT.jar /tmp/partitioning-consumer-kafka.jar
+cp partitioning-producer-sample/target/partitioning-producer-sample-*-kafka.jar /tmp/partitioning-producer-kafka.jar
+cp partitioning-consumer-sample-kafka/target/partitioning-consumer-sample-kafka-*-SNAPSHOT.jar /tmp/partitioning-consumer-kafka.jar
 
-./mvnw clean package -U -DskipTests -P rabbit-binder -pl :partitioning-producer
+./mvnw clean package -U -DskipTests -P rabbit-binder -pl :partitioning-producer-sample
 
-cp partitioning-producer/target/partitioning-producer-*-SNAPSHOT.jar /tmp/partitioning-producer-rabbit.jar
-cp partitioning-consumer-rabbit/target/partitioning-consumer-rabbit-*-SNAPSHOT.jar /tmp/partitioning-consumer-rabbit.jar
+cp partitioning-producer-sample/target/partitioning-producer-sample-*-rabbit.jar /tmp/partitioning-producer-rabbit.jar
+cp partitioning-consumer-sample-rabbit/target/partitioning-consumer-sample-rabbit-*-SNAPSHOT.jar /tmp/partitioning-consumer-rabbit.jar
 
 popd
 
