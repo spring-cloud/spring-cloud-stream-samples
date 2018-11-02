9beb9b73522ce1d93c78c5b5193c54bd878ba439
Soby Chacko <schacko@pivotal.io>
2018-11-02 09:50:39 -0400

    Add new sample for multiple binders
    Demonstrate how a Kafka Streams processor can be used to
    produce data and then publish that into Rabbitmq.

---
+++ kafka-streams-samples/kafka-streams-to-rabbitmq-message-channel/src/main/java/kafka/streams/message/channel/KafkaStreamsWordCountApplication.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package kafka.streams.message.channel;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Serialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.SubscribableChannel;
+import org.springframework.messaging.handler.annotation.SendTo;
+
+import java.util.Arrays;
+import java.util.Date;
+
+@SpringBootApplication
+public class KafkaStreamsWordCountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStreamsWordCountApplication.class, args);
+	}
+
+	@EnableBinding(MultipleProcessor.class)
+	public static class WordCountProcessorApplication {
+
+		@StreamListener("kstreamIn")
+		@SendTo("kstreamOut")
+		public KStream<?, WordCount> process(KStream<Object, String> input) {
+
+			return input
+					.flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")))
+					.map((key, value) -> new KeyValue<>(value, value))
+					.groupByKey(Serialized.with(Serdes.String(), Serdes.String()))
+					.windowedBy(TimeWindows.of(20_000))
+					.count(Materialized.as("WordCounts-1"))
+					.toStream()
+					.map((key, value) -> new KeyValue<>(null, new WordCount(key.key(), value, new Date(key.window().start()), new Date(key.window().end()))));
+		}
+
+		@StreamListener("fromKafka")
+		@SendTo("toRabbit")
+		public WordCount sink(WordCount input) {
+			return input;
+		}
+
+		@StreamListener("testInputFromRabbit")
+		public void receive(String data) {
+			System.out.println("Data received..." + data);
+		}
+
+	}
+
+	interface MultipleProcessor {
+
+		String KSTREAM_IN = "kstreamIn";
+		String KSTREAM_OUT = "kstreamOut";
+
+		String FROM_KAFKA = "fromKafka";
+		String TO_RABBIT = "toRabbit";
+
+		String TEST_INPUT_FROM_RABBIT = "testInputFromRabbit";
+
+		@Input(KSTREAM_IN)
+		KStream<?, ?> kstreamIn();
+
+		@Output(KSTREAM_OUT)
+		KStream<?, ?> kstreamOut();
+
+		@Input(FROM_KAFKA)
+		SubscribableChannel fromKafka();
+
+		@Output(TO_RABBIT)
+		MessageChannel toRabbit();
+
+		@Input(TEST_INPUT_FROM_RABBIT)
+		SubscribableChannel testInputFromRabbit();
+	}
+
+	static class WordCount {
+
+		private String word;
+
+		private long count;
+
+		private Date start;
+
+		private Date end;
+
+		WordCount(String word, long count, Date start, Date end) {
+			this.word = word;
+			this.count = count;
+			this.start = start;
+			this.end = end;
+		}
+
+		public String getWord() {
+			return word;
+		}
+
+		public void setWord(String word) {
+			this.word = word;
+		}
+
+		public long getCount() {
+			return count;
+		}
+
+		public void setCount(long count) {
+			this.count = count;
+		}
+
+		public Date getStart() {
+			return start;
+		}
+
+		public void setStart(Date start) {
+			this.start = start;
+		}
+
+		public Date getEnd() {
+			return end;
+		}
+
+		public void setEnd(Date end) {
+			this.end = end;
+		}
+	}
+}
---
+++ kafka-streams-samples/kafka-streams-to-rabbitmq-message-channel/src/test/java/kafka/streams/message/channel/KafkaStreamsWordCountApplicationTests.java
@@ -0,0 +1,18 @@
+package kafka.streams.message.channel;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KafkaStreamsWordCountApplicationTests {
+
+	@Test
+	@Ignore
+	public void contextLoads() {
+	}
+
+}
