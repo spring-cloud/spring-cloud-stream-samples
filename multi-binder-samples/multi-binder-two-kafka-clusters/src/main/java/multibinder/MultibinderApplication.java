dad51e3d400e64b582f12cd94dd656812e628302
Soby Chacko <schacko@pivotal.io>
2019-10-30 11:27:55 -0400

    Refactoring mult-binder-two-kafka-clusters sample

--- multi-binder-samples/multi-binder-two-kafka-clusters/src/main/java/multibinder/MultibinderApplication.java
+++ multi-binder-samples/multi-binder-two-kafka-clusters/src/main/java/multibinder/MultibinderApplication.java
@@ -16,14 +16,47 @@
 
 package multibinder;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MultibinderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MultibinderApplication.class, args);
+	}
+
+	@Bean
+	public Function<String, String> process() {
+		return payload -> payload.toUpperCase();
+	}
+
+	static class TestProducer {
+
+		private AtomicBoolean semaphore = new AtomicBoolean(true);
+
+		@Bean
+		public Supplier<String> sendTestData() {
+			return () -> this.semaphore.getAndSet(!this.semaphore.get()) ? "foo" : "bar";
+		}
+	}
+
+	static class TestConsumer {
+
+		private final Log logger = LogFactory.getLog(getClass());
+
+		@Bean
+		public Consumer<String> receive() {
+			return s -> logger.info("Data received..." + s);
+		}
 	}
 
 }
