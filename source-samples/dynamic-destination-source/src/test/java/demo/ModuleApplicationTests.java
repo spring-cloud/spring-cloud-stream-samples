a7875c7f7476780ce6748f7e4bcd231b70b02dd7
Soby Chacko <schacko@pivotal.io>
2019-10-31 13:54:18 -0400

    Dynamic destination refactoring

--- source-samples/dynamic-destination-source/src/test/java/demo/ModuleApplicationTests.java
+++ source-samples/dynamic-destination-source/src/test/java/demo/ModuleApplicationTests.java
@@ -25,7 +25,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = SourceApplication.class)
+@SpringBootTest(classes = DynamicDestinationSourceApplication.class)
 @WebAppConfiguration
 @DirtiesContext
 public class ModuleApplicationTests {
