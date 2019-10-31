f9f3b10237b2091638e2d4f3b633c247f61158cc
Soby Chacko <schacko@pivotal.io>
2019-10-30 21:31:03 -0400

    Removing source samples/jdbc-source from samples repo
    Removing source samples/jdbc-source from samples repo in favor of the JDBC app starters.
    https://github.com/spring-cloud-stream-app-starters/jdbc

--- source-samples/jdbc-source/src/main/resources/sample-schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE test;
-create table test(
-  id bigint,
-  name varchar (2000),
-  tag char(1)
-);
-insert into test values (1, 'Bob', NULL);
-insert into test values (2, 'Jane', NULL);
-insert into test values (3, 'John', NULL);
